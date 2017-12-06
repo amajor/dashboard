045629495d6dbd0a074edfa1e8613ccba25c9354
Alison Major <alison.major@gmail.com>
2017-12-06 15:42:39 -0500

    Revise to pass tests

--- features/step_definitions/header.js
+++ features/step_definitions/header.js
@@ -1,15 +1,24 @@
-const {defineSupportCode: cucumber} = require(`cucumber`);
+const puppeteer = require('puppeteer');
+const { defineSupportCode } = require('cucumber')
 
-cucumber(({Given, Then}) => {
+defineSupportCode(({ Before, Given, When, Then }) => {
+  Before({ timeout: 60 * 1000 }, async function testCase() {
+    this.browser = await puppeteer.launch()
+  })
 
-  Given('I am viewing the dashboard', function (int, callback) {
-    // Write code here that turns the phrase above into concrete actions
-    callback(null, 'pending');
-  });
+  Given('I am viewing the dashboard', { timeout: 60 * 1000 }, async function testCase() {
+    this.page = await this.browser.newPage()
+    await this.page.goto('https://amajor.github.io/dashboard/public/')
+  })
 
-  Then('I see the header', function (int, callback) {
-    // Write code here that turns the phrase above into concrete actions
-    callback(null, 'pending');
-  });
+  Then('I see the header', async function testCase() {
+    await this.page.waitForSelector('h1')
 
-});
+    const headers = await this.page.evaluate(() => {
+      const h1s = Array.from(document.querySelectorAll('h1'))
+      return h1s.map(title => title.textContent)
+    })
+
+    await this.browser.close()
+  })
+})
